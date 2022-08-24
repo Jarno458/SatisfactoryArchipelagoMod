b4004de2cfd745bc8c70f9ef780e6fe33ced149d
Jarno Westhof <jarnowesthof@gmail.com>
2022-08-24 13:49:39 +0200

    Added AP subsystem

---
+++ Source/Archipelago/Private/ApSubsystem.cpp
@@ -0,0 +1,48 @@
+#include "ApSubsystem.h"
+
+DEFINE_LOG_CATEGORY(ApSubsystem);
+
+// Sets default values
+AApSubsystem::AApSubsystem() : Super()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AApSubsystem::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UE_LOG(ApSubsystem, Display, TEXT("AApSubsystem::BeginPlay()"));
+
+	SManager = AFGSchematicManager::Get(GetWorld());
+	RManager = AFGResearchManager::Get(GetWorld());
+
+	AP_Init("localhost:38281", "Timespinner", "Jarno", "");
+
+	AP_SetItemClearCallback([]() {});
+	AP_SetItemRecvCallback([](int id, bool notify) {
+		});
+	AP_SetLocationCheckedCallback([](int id) {
+		});
+
+	AP_Start();
+
+	//GetWorldTimerManager().SetTimer(timerHandle, this, &AApSubsystem::SecondThick, 1.0f, true, 2.0f);
+}
+
+void AApSubsystem::SecondThick() {
+	UE_LOG(ApSubsystem, Display, TEXT("AApSubsystem::SecondThick()"));
+
+	//AP_SetServerDataRaw("EnergyLink", "add", "10", "0");
+}
+
+
+// Called every frame
+void AApSubsystem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/Archipelago/Public/ApSubsystem.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "FGSchematicManager.h"
+#include "FGResearchManager.h"
+
+#include "Patching/BlueprintHookHelper.h"
+#include "Patching/BlueprintHookManager.h"
+#include "Registry/ModContentRegistry.h"
+#include "Util/BlueprintLoggingLibrary.h"
+#include "Subsystem/ModSubsystem.h"
+
+#include "Archipelago.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(ApSubsystem, Log, All);
+
+#include "ApSubsystem.generated.h"
+
+UCLASS(Blueprintable)
+class ARCHIPELAGO_API AApSubsystem : public AModSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AApSubsystem();
+
+	UPROPERTY()
+		AFGSchematicManager* SManager;
+	UPROPERTY()
+		AFGResearchManager* RManager;
+
+	FTimerHandle timerHandle;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	void SecondThick();
+
+};
